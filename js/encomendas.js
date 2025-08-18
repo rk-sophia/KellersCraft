664e3c2998309d66f5af0d55fe53804789508361
Sophia Ramalho Keller <130664417+rk-sophia@users.noreply.github.com>
2025-08-18 19:25:27 -0300

    qasrf

--- js/encomendas.js
+++ js/encomendas.js
@@ -1,1 +1,10 @@
-document.querySelector(".qtde").textContent * document.querySelector(".unitario").textContent
+// document.querySelector(".total").textContent = document.querySelector(".qtde").textContent * document.querySelector(".unitario").textContent
+// Captura as encomendas da tabela
+var clientes = document.querySelectorAll(".cliente");
+
+// Passa por todas as encomendas e calcula o valor total
+for (var count=0; count < clientes.length; count ++){
+    clientes[count].querySelector(".total").textContent = 
+    clientes[count].querySelector(".qtde").textContent * 
+    clientes[count].querySelector(".unitario").textContent
+}
